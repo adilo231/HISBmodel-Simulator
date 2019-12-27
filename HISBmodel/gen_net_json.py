bd183bef93b5ea30da19104d0c81de7adb9f1087
hosni adil imad eddine <adilo231@users.noreply.github.com>
2019-12-27 14:43:47 +0800

    last

--- HISBmodel/gen_net_json.py
+++ HISBmodel/gen_net_json.py
@@ -3,9 +3,9 @@ import sys, json
 import networkx as nx
 from networkx.readwrite import json_graph
 
-g = nx.barabasi_albert_graph(300, 2)
-
-#g = nx.grid_3d_graph(10,10)
+#g = nx.barabasi_albert_graph(1000, 3)
+#g= nx.watts_strogatz_graph(500,20,0.2)
+g= nx.connected_caveman_graph(15, 5)
 
 nodes = g.nodes()
 
